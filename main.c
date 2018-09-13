89a312cd747584dffcba02355668f140d5702a2e
Andrea Ford <ac123103@ohio.edu>
2018-09-13 18:51:01 -0400

    first commit

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //Variable declaration and assignment.
+    int a;
+    a = 445;
+
+    //Variable declaration and initialization.
+    int b = 4;
+    int c = a;
+
+
+    printf("Value of a = %d \nValue of b = %d \nValue of c = %d \n",a,b,c);
+
+
+
+
+
+    //printf("Hello world!\n");
+    return 0;
+}
---
+++ notes9_13_18.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //Variable declaration and assignment.
+    int a;
+    a = 445;
+
+    //Variable declaration and initialization.
+    int b = 4;
+    int c = a;
+
+
+    printf("Value of a = %d \nValue of b = %d \nValue of c = %d \n",a,b,c);
+
+    /*
+    notes click and drag to here if I want to
+
+    */
+
+
+
+
+
+    //printf("Hello world!\n");
+    return 0;
+}
